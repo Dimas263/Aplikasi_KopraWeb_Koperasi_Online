fa7ce4c0ba00ccca54bb5c616cbb3c1546b08ccc
Dimas Dwi Putra <dwiputradimas123@gmail.com>
2022-08-10 17:35:40 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/app/koperasi/';
+$config['base_url'] = 'https://webku.one/app/koperasi/';
 $config['index_page'] = '';
 $config['uri_protocol']	= 'REQUEST_URI';
 $config['url_suffix'] = '';
